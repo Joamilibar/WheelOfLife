b19e8cd8c2dd7d0912f6ed967c382e38beb0b8c3
Joamilibar <99300599+Joamilibar@users.noreply.github.com>
2025-02-03 22:38:04 -0300

    Colores Barra deslizante y cambio posición rueda

--- src/WheelOfLife.js
+++ src/WheelOfLife.js
@@ -41,72 +41,111 @@ const WheelOfLife = () => {
     const radius = 100;
 
     return (
-        <div className="wheel-container">
-            <img src={logo} className="App-logo" alt="logo" />
-            <h1>Rueda de la Vida</h1>
+        <div className="App">
+            <div className="wheel-container">
+                <img src={logo} className="App-logo" alt="logo" />
+                <h1>Rueda de la Vida</h1>
 
-            {/* Mostrar la rueda solo si `showWheel` es verdadero */}
-            {showWheel && (
-                <svg viewBox="0 0 200 200" width="300" height="300" className="wheel">
-                    {[...Array(numSections)].map((_, sectionIndex) => {
-                        const angle = (sectionIndex * 360) / numSections; // Ángulo de cada sección
-                        const rotation = `rotate(${angle} 100 100)`; // Rotación para la sección
 
-                        return (
-                            <g key={sectionIndex} transform={rotation}>
-                                {/* Dividir cada sección en 10 partes concéntricas */}
-                                {[...Array(numLevels)].map((_, levelIndex) => {
-                                    const innerRadius = (radius / numLevels) * levelIndex;
-                                    const outerRadius = (radius / numLevels) * (levelIndex + 1);
+                {/* Siempre mostrar los controles deslizantes con los nombres */}
+                <div className="sliders">
+                    {ratings.map((rating, index) => (
+                        <div key={index} className="slider">
+                            <label>{sectionNames[index]}: </label> {/* Mostrar el nombre de la sección */}
+                            <input
+                                type="range"
+                                min="0"
+                                max="10"
+                                value={rating}
+                                onChange={(e) => handleRatingChange(index, parseInt(e.target.value))}
+                                style={{
+                                    accentColor: colors[index].replace('0.2', '1'), // Color de acento para el control deslizante
+                                    backgroound: colors[index], // Color de fondo para el control deslizante
+                                }}
+                            />
+                            <span>{rating}</span>
+                        </div>
+                    ))}
+                </div>
+                <div className="button-container">
 
-                                    const isSelected = ratings[sectionIndex] >= levelIndex + 1;
+                    {/* Botón para mostrar la rueda */}
+                    <button onClick={() => setShowWheel(!showWheel)}>
+                        {showWheel ? 'Ocultar Rueda' : 'Mostrar Rueda'}
+                    </button>
+                </div>
+                <div>
 
-                                    const fillColor = isSelected
-                                        ? colors[sectionIndex].replace('0.2', '1') // Color más oscuro al seleccionar
-                                        : colors[sectionIndex]; // Color predeterminado
+                    {/* Mostrar la rueda solo si `showWheel` es verdadero */}
+                    {showWheel && (
+                        <svg viewBox="0 0 200 200" width="600" height="600" className="wheel">
+                            {[...Array(numSections)].map((_, sectionIndex) => {
+                                const angle = (sectionIndex * 360) / numSections; // Ángulo de cada sección
+                                const rotation = `rotate(${angle} 100 100)`; // Rotación para la sección
 
-                                    return (
-                                        <path
-                                            key={levelIndex}
-                                            d={`
-                        M 100,100 
-                        L ${100 + innerRadius * Math.cos(0)},${100 + innerRadius * Math.sin(0)}
-                        A ${innerRadius},${innerRadius} 0 0,1 ${100 + innerRadius * Math.cos(Math.PI / 4)},${100 + innerRadius * Math.sin(Math.PI / 4)}
-                        L ${100 + outerRadius * Math.cos(Math.PI / 4)},${100 + outerRadius * Math.sin(Math.PI / 4)}
-                        A ${outerRadius},${outerRadius} 0 0,0 ${100 + outerRadius * Math.cos(0)},${100 + outerRadius * Math.sin(0)}
-                        Z
-                      `}
-                                            fill={fillColor}
-                                            style={{ cursor: 'default' }}
-                                        />
-                                    );
-                                })}
-                            </g>
-                        );
-                    })}
-                </svg>
-            )}
+                                return (
+                                    <g key={sectionIndex} transform={rotation}>
+                                        {/* Dividir cada sección en 10 partes concéntricas */}
+                                        {[...Array(numLevels)].map((_, levelIndex) => {
+                                            const innerRadius = (radius / numLevels) * levelIndex;
+                                            const outerRadius = (radius / numLevels) * (levelIndex + 1);
 
-            {/* Siempre mostrar los controles deslizantes con los nombres */}
-            <div className="sliders">
-                {ratings.map((rating, index) => (
-                    <div key={index} className="slider">
-                        <label>{sectionNames[index]}: </label> {/* Mostrar el nombre de la sección */}
-                        <input
-                            type="range"
-                            min="0"
-                            max="10"
-                            value={rating}
-                            onChange={(e) => handleRatingChange(index, parseInt(e.target.value))}
-                        />
-                        <span>{rating}</span>
-                    </div>
-                ))}
+                                            const isSelected = ratings[sectionIndex] >= levelIndex + 1;
+
+                                            const fillColor = isSelected
+                                                ? colors[sectionIndex].replace('0.2', '1') // Color más oscuro al seleccionar
+                                                : colors[sectionIndex]; // Color predeterminado
+
+                                            return (
+                                                <path
+                                                    key={levelIndex}
+                                                    d={`
+                                                M 100,100 
+                                                L ${100 + innerRadius * Math.cos(0)},${100 + innerRadius * Math.sin(0)}
+                                                A ${innerRadius},${innerRadius} 0 0,1 ${100 + innerRadius * Math.cos(Math.PI / 4)},${100 + innerRadius * Math.sin(Math.PI / 4)}
+                                                L ${100 + outerRadius * Math.cos(Math.PI / 4)},${100 + outerRadius * Math.sin(Math.PI / 4)}
+                                                A ${outerRadius},${outerRadius} 0 0,0 ${100 + outerRadius * Math.cos(0)},${100 + outerRadius * Math.sin(0)}
+                                                Z
+                                                `}
+                                                    fill={fillColor}
+                                                    style={{ cursor: 'default' }}
+                                                />
+                                            );
+                                        })}
+
+                                    </g>
+                                );
+                            })}
+
+                            {/* Agregar nombres fuera de la rueda */}
+                            {sectionNames.map((name, sectionIndex) => {
+                                const angle = (sectionIndex * 360) / numSections;
+                                const offset = 50; // Distancia del texto al borde de la rueda
+
+                                const textX = 100 + offset * Math.cos((angle - 90) * (Math.PI / 180));
+                                const textY = 100 + offset * Math.sin((angle - 90) * (Math.PI / 180));
+
+                                return (
+                                    <text
+                                        key={sectionIndex}
+                                        x={textX}
+                                        y={textY}
+                                        fill="black"
+                                        fontSize="6"
+                                        fontWeight="bold"
+                                        textAnchor="middle"
+                                        dominantBaseline="right"
+                                        transform={`rotate(${angle + 90}, ${textX}, ${textY})`} // Ajustar rotación del texto
+                                    >
+                                        {name}
+                                    </text>
+                                );
+                            })}
+
+                        </svg>
+                    )}
+                </div>
             </div>
-            {/* Botón para mostrar la rueda */}
-            <button onClick={() => setShowWheel(!showWheel)}>
-                {showWheel ? 'Ocultar Rueda' : 'Mostrar Rueda'}
-            </button>
         </div>
     );
 };
